21287680691994d8af50d2190f9a0dab88cf30d0
renanrod4 <renanrdemeneses@gmail.com>
2026-04-03 18:55:18 -0300

    feat(seção de destaques): adiciona componente HighlightsSection com cards informativos

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import './page.css';
 
 import { Header } from '../components/header/Header';
 import { InitialSection } from '../components/initial-section/InitialSection';
+import HighlightsSection from '@/components/highlights-section/page';
 
 export default function Home() {
 	return (
 		<main className="w-full">
 			<Header />
 			<InitialSection />
+			<HighlightsSection />
 		</main>
 	);
 }
---
+++ components/highlights-section/page.tsx
@@ -0,0 +1,43 @@
+import { FaLocationDot } from 'react-icons/fa6';
+import { FaClock } from 'react-icons/fa';
+import { FaTruck } from 'react-icons/fa';
+import { FaShieldAlt } from 'react-icons/fa';
+import { IconType } from 'react-icons';
+
+export default function HighlightsSection() {
+	return (
+		<section className="py-16 px-4 md:px-16 bg-gray-100">
+			<Cards />
+		</section>
+	);
+}
+
+function Cards() {
+	return (
+		<div className="flex gap-6">
+			<Card icon={FaLocationDot} title='São Paulo e Região' description='Atendemos toda a região metropolitana de São Paulo com rapidez e eficiência.' />
+			<Card icon={FaClock} title='2h de Chegada' description='Chegamos ao local em até 2 horas após o contato' />
+			<Card icon={FaTruck} title='Visita Gratuita' description='Realizamos uma visita técnica gratuita para avaliar o problema e fornecer um orçamento preciso.' />
+			<Card icon={FaShieldAlt} title='Análise Profissional' description='Preço justo definido somente após análise no local' />
+		</div>
+	);
+}
+
+function Card({ icon, title, description }: { icon?: IconType; title?: string; description?: string }) {
+	const Icon = icon;
+	return (
+		<div className="flex flex-col items-center">
+			<div className="flex items-center  gap-4 p-5 bg-brand-primary rounded-2xl shadow-md">
+				{Icon ? (
+					<Icon className="fill-text-on-blue" size={40} />
+				) : (
+					<div className="w-12 h-12 bg-gray-300 rounded-full" />
+				)}
+			</div>
+			<h3 className="text-lg font-semibold mt-4">{title || 'Título do destaque'}</h3>
+			<p className="text-gray-600 mt-2">
+				{description || 'Descrição do destaque para explicar o benefício ou característica.'}
+			</p>
+		</div>
+	);
+}
