df3dd3f2c3a08917bba0b30784411b616da515e7
renanrod4 <renanrdemeneses@gmail.com>
2026-04-03 17:08:34 -0300

    refactor(header): altera estrutura de Header e NavItems para melhor semântica

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -4,12 +4,12 @@ import { WhatsappBtn } from '../WhatsappBtn';
 
 export function Header() {
 	return (
-		<nav className="fixed top-0 left-0 right-0 flex h-16 mx-4 md:mx-[12%]">
+		<header className="fixed top-0 left-0 right-0 flex h-16 mt-4 mx-2 md:mx-[2%]">
 			<ul className="flex w-full h-full gap-8 px-2 items-center">
 				<Logo />
 				<NavItems />
 				<li><WhatsappBtn /></li>
 			</ul>
-		</nav>
+		</header>
 	);
 }
--- components/header/NavItems.tsx
+++ components/header/NavItems.tsx
@@ -9,12 +9,12 @@ const menuItems = [
 
 export function NavItems() {
 	return (
-		<>
+		<nav className="flex gap-6">
 			{menuItems.map((item) => (
-				<li key={item.label} className='flex items-center text-text-on-blue	'>
+				<li key={item.label} className='flex items-center text-text-on-blue	hover:text-highlight-light-blue transition-colors'>
 					<Link href={item.href}>{item.label}</Link>
 				</li>
 			))}
-		</>
+		</nav>
 	);
 }
